93f657db935e323c33c13c29e9744cbe279ec909
Youssef Boulhilat <yboulhilat@gmail.com>
2019-10-17 11:00:07 +0200

    Redirect ok

--- src/App.js
+++ src/App.js
@@ -10,14 +10,8 @@ import {
   Switch,
   Route,
 } from "react-router-dom";
-const API = 'http://gateway.marvel.com/v1/public/characters?apikey=9017949d28693c6128932224d13271c5'
 class App extends React.Component {
-  componentWillMount(){
-    fetch(API).then((resp) => resp.json())
-      .then(data => {
-        console.log('Movies fetch ====>', data)
-      })
-  }
+ 
   render() {
     return (
       <Container fixed>
